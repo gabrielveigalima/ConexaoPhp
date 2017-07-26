2d5e575d3e49ab2cc90f651e8a05b3ea25599820
Gabriel Lima <gabrielveigalima@icloud.com>
2017-07-25 23:10:19 -0300

    Conexao com o banco de dados comentado.

--- conexao.php
+++ conexao.php
@@ -1,15 +1,18 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$pws = "";
-$banco = "ideia";
+$servidor   = "localhost";//Nome do servidor
+$usuario    = "root";//Nome do usuário
+$pws        = "";//senha
+$banco      = "ideia";//Nome do Bando de dados
 
-
+//Variável de conexão
 $conn = mysqli_connect($servidor,$usuario,$pws,$banco);
 
+//Verifica se a conexão foi realizada
 if($conn){
-	echo "ok";
+    //Se conectar ele vai escrever isso, mas isso deve ser tiradado depois que o teste de conexão for realizado
+    echo "ok";
 }else{
+    //Se não conectar ele vai matar e escrever o erro
 	die("Não conectou ao bd ".mysqli_connect_error());
 }
 ?>
